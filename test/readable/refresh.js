fe75e2a587bec60de4939ad854af8f85cdeec436
Dian Fay <dian.m.fay@gmail.com>
2019-05-19 14:11:22 -0400

    test: basic coverage for concurrent matview refresh

--- test/readable/refresh.js
+++ test/readable/refresh.js
@@ -3,11 +3,11 @@
 describe('refresh', function () {
   let db;
 
-  before(function () {
+  beforeEach(function () {
     return resetDb('views').then(instance => db = instance);
   });
 
-  after(function () {
+  afterEach(function () {
     return db.instance.$pool.end();
   });
 
@@ -23,6 +23,24 @@ describe('refresh', function () {
     assert.equal(afterWithoutRefresh, 2);
 
     yield db.vals_ending_with_e.refresh();
+
+    const afterWithRefresh = yield db.vals_ending_with_e.count({});
+
+    assert.equal(afterWithRefresh, 3);
+  });
+
+  it('refreshes concurrently', function* () {
+    const before = yield db.vals_ending_with_e.count({});
+
+    assert.equal(before, 2);
+
+    yield db.vals.insert({string: 'five'});
+
+    const afterWithoutRefresh = yield db.vals_ending_with_e.count({});
+
+    assert.equal(afterWithoutRefresh, 2);
+
+    yield db.vals_ending_with_e.refresh(true);
 
     const afterWithRefresh = yield db.vals_ending_with_e.count({});
 
